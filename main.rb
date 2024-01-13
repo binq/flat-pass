9549a2bbef905b94c08ed3dfa7b8679fc4b9b710
binq <37637+binq@users.noreply.github.com>
2024-01-13 04:30:06 +0000

    Initial

--- main.rb
+++ main.rb
@@ -1,20 +1,21 @@
+require 'openssl'
 require 'yaml'
 
 class Password
-  attr :password_length
-  attr :word_length
-  attr :separator
+  attr_reader :password_length
+  attr_reader :word_length
+  attr_reader :separator
   
   def initialize(
     password_length: 24,
-    word_length: {min: 3, max: 7},
+    word_length: 3..7,
     separator: '-'
   )
     @password_length = password_length
     @word_length = word_length
     @separator = separator
 
-    word_length => {min:, max:}
+    (min, max) = %i(first last).map(&word_length.method(:public_send))
 
     raise "invalid password length: #{password_length}" unless password_length > 0
     raise "min word length can't be greater than max (#{min}, #{max})" unless min <= max
@@ -27,60 +28,60 @@ class Password
       of the password length (#{password_length})
     EOM
   end
-        
-  def generate_char(_)
-    (97 + rand(26)).chr
-  end
-  
-  def generate_word(l, i)
-    try = (0...l).map(&method(:generate_char)).join('')
 
-    case
-    when 0 == i
-      num = "%02u" % rand(100)
-      result = "#{try.slice(0, 1).capitalize}#{num}#{try.slice(3, try.length)}"
-    else
-      result = try
+  class_eval do
+    first_char = 'a'.ord
+    define_method :generate_chars do |l|
+      OpenSSL::Random.random_bytes(l).each_byte.map { |x| (first_char + (x % 26)).chr }.join
     end
-
-    result
   end
 
-  def decide_word_lengths(range, l)
-    loop do
+  def generate_word(l, i) =
+    generate_chars(l).then do |f|
       case
-      when l.zero?
-        return
-      when l > (2 + range.max + range.max)
-        n = rand(range)
-        yield n
-        l -= n + 1
+      when i == 0 && l < 7
+        x = l - 1
+        t = "%0#{x}u" 
+        "#{f.slice(0, 1).capitalize}#{t % rand(10**x)}"
+      when i == 0
+        x = 2
+        t = "%0#{x}u" 
+        "#{f.slice(0, 1).capitalize}#{t % rand(10**x)}-#{f.slice(4, l)}"
       else
-        n1 = (l / 2.0).floor
-        yield n1
-        l -= n1 + 1
-
-        n2 = l
-        yield n2
-        l -= n2
+        f
       end
     end
-  end
 
-  def generate_password
-    word_length => {min:, max:}
+  def decide_word_lengths = 
+    password_length.then do |l|
+      loop do
+        case
+        when l.zero?
+          return
+        when l > (2 * word_length.max + 2)
+          n = rand(word_length)
+          yield n
+          l -= n + 1
+        else
+          n1 = (l / 2.0).floor
+          yield n1
+          l -= n1 + 1
+          n2 = l
+          yield n2
+          l -= n2
+        end
+      end
+    end
 
-    enum_for(:decide_word_lengths, min..max, password_length)
+  def generate_password =
+    enum_for(:decide_word_lengths)
       .each_with_index
       .map(&method(:generate_word))
       .join(separator)
-  end
 
   alias_method :to_s, :generate_password
 end
 
-puts "Hello, World!"
 (0...100).each do |i|
-  passwd = Password.new
-  puts "#{"%02u" % i}: #{passwd}"
+  puts "%02u: %s" % [i, Password.new]
 end
